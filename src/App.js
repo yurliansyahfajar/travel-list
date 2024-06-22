f14b25070fb07e1aa30518799b7dc97b211fc98f
yurliansyahf <yurliansyahf@gmail.com>
2024-06-22 21:14:47 +0700

    add sorting and clear all item function

--- src/App.js
+++ src/App.js
@@ -24,6 +24,11 @@ export default function App() {
     );
   };
 
+  const handleDeleteAllItem = () => {
+    const confirm = window.confirm("Are you sure want to clear all items?");
+    if (confirm) setItems([]);
+  };
+
   return (
     <div className="app">
       <Logo />
@@ -32,6 +37,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onCheckItem={handleCheckItem}
+        onDeleteAllItem={handleDeleteAllItem}
       />
       <Stats items={items} />
     </div>
@@ -93,23 +99,49 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onCheckItem }) {
+function PackingList({ items, onDeleteItem, onCheckItem, onDeleteAllItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItem;
+
+  if (sortBy === "input") sortedItem = items;
+
+  if (sortBy === "description")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className="list">
-      {items.length !== 0 ? (
-        <ul>
-          {items.map((item) => (
-            <Item
-              description={item.description}
-              quantity={item.quantity}
-              packed={item.packed}
-              id={item.id}
-              key={item.id}
-              onDeleteItem={onDeleteItem}
-              onCheckItem={onCheckItem}
-            />
-          ))}
-        </ul>
+      {sortedItem.length !== 0 ? (
+        <>
+          <ul>
+            {sortedItem.map((item) => (
+              <Item
+                description={item.description}
+                quantity={item.quantity}
+                packed={item.packed}
+                id={item.id}
+                key={item.id}
+                onDeleteItem={onDeleteItem}
+                onCheckItem={onCheckItem}
+              />
+            ))}
+          </ul>
+          <div className="actions">
+            <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+              <option value="input">Sort by input order</option>
+              <option value="description">Sort by description</option>
+              <option value="packed">Sort by packed status</option>
+            </select>
+            <button onClick={onDeleteAllItem}>Clear List</button>
+          </div>
+        </>
       ) : (
         <div>
           <h3>Packing List is Empty...</h3>
